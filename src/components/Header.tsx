68973503ec3ae7d5939fa881acc6a4c8a51c48a1
hina <hinadegozaru888@gmail.com>
2025-05-24 18:38:31 +0900

    ヘッダーを修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="absolute top-0 right-0 z-10 max-w-5xl">
       <nav className="flex gap-4 p-4 font-bold text-lg">
-        <a href="#works" className="hover:underline">
+        <Link href="/" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/#works" className="hover:underline">
           Works
-        </a>
-        <a href="#profile" className="hover:underline">
+        </Link>
+        <Link href="/#profile" className="hover:underline">
           Profile
-        </a>
+        </Link>
+        {/* <Link href="/3d-works" className="hover:underline">
+          3DWorks
+        </Link> */}
       </nav>
     </header>
   );
