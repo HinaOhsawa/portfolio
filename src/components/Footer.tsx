eb3e786cd31e5e97d5125af35a80556597ab202b
hina <hinadegozaru888@gmail.com>
2025-06-03 18:17:45 +0900

    フッターを作成

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className="flex flex-col w-9/10 mx-auto py-8">
+      <p className="text-center text-sm font-bold mb-4">Thank you!</p>
+      <div className="text-center text-sm text-gray-500 mb-8">
+        &copy; 2025 Ohsawa Hina. All rights reserved.
+      </div>
+    </footer>
+  );
+}
