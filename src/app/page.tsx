6b11960529d48c65fc21d6c308a734917a5cdb8b
hina <hinadegozaru888@gmail.com>
2025-06-03 16:00:08 +0900

    ヘッダーの表示を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,9 @@ import Works from "@/components/Works";
 export default function HomePage() {
   return (
     <>
+      <Header />
       <MainVisual />
       <div className="mx-auto max-w-5xl py-8 px-4 sm:px-6">
-        <Header />
         <Works />
         <Profile />
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="absolute top-0 right-0 z-10 max-w-5xl">
-      <nav className="flex gap-4 p-4 font-bold text-lg">
+    <header className="flex flex-col w-9/10 mx-auto ">
+      {/* <header className="absolute top-0 right-0 z-10 max-w-5xl"> */}
+      <nav className="justify-end flex gap-4 p-4 font-bold text-lg">
         <Link href="/" className="hover:underline">
           Home
         </Link>
@@ -13,9 +14,6 @@ export default function Header() {
         <Link href="/#profile" className="hover:underline">
           Profile
         </Link>
-        {/* <Link href="/3d-works" className="hover:underline">
-          3DWorks
-        </Link> */}
       </nav>
     </header>
   );
